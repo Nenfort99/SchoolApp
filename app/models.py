e81077283126cfea601ae80d9d8591141b8c883e
Nenfort99 <msnenfort99@gmail.com>
2024-09-19 20:41:19 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from tortoise import fields
 from tortoise.models import Model
-from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_creator import pydantic_requirements_creator, pydantic_queryset_creator
+from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_creator import pydantic_items_creator, pydantic_queryset_creator
 from datetime import datetime
 
 class Course(Model):
@@ -52,7 +52,7 @@ Blog_Pydantic = pydantic_model_creator(Blogpost, name="Blog")
 BlogIn_Pydantic = pydantic_model_creator(Blogpost, name="BlogIn", exclude_readonly=True)
 
 
-class Requirements(Model):
+class Items(Model):
     id = fields.IntField(pk=True)  # Primary key
     title = fields.CharField(max_length=255)  # Post title
     body = fields.TextField()  # Post content
@@ -67,13 +67,13 @@ class Requirements(Model):
         return self.title
 
     class PydanticMeta:
-        table = "requirements"  # Set the table name in the database
+        table = "item"  # Set the table name in the database
 
 
 
 
-Requirements_Pydantic = pydantic_model_creator(Requirements, name="Requirements")
-RequirementsIn_Pydantic = pydantic_model_creator(Requirements, name="RequirementsIn", exclude_readonly=True)
+Items_Pydantic = pydantic_model_creator(Items, name="Items")
+ItemsIn_Pydantic = pydantic_model_creator(Items, name="ItemsIn", exclude_readonly=True)
 
 
 TORTOISE_ORM = {
